46befcba9a541070d97bc0bc826cfe8507a75f72
Mark Phelps <mark.aaron.phelps@gmail.com>
2022-06-23 21:34:45 -0400

    WIP flag cache

--- go.mod
+++ go.mod
@@ -44,7 +44,6 @@ require (
 	github.com/Microsoft/go-winio v0.4.17 // indirect
 	github.com/Microsoft/hcsshim v0.8.23 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
 	github.com/cenkalti/backoff/v4 v4.1.2 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/containerd/cgroups v1.0.1 // indirect
@@ -92,7 +91,6 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/objx v0.4.0 // indirect
 	github.com/subosito/gotenv v1.3.0 // indirect
-	github.com/testcontainers/testcontainers-go v0.13.0 // indirect
 	github.com/uber/jaeger-lib v2.2.0+incompatible // indirect
 	github.com/urfave/negroni v1.0.0 // indirect
 	github.com/vmihailenco/go-tinylfu v0.2.2 // indirect
--- server/flag.go
+++ server/flag.go
@@ -2,19 +2,69 @@ package server
 
 import (
 	"context"
+	"crypto/md5"
+	"fmt"
 
 	flipt "go.flipt.io/flipt/rpc/flipt"
 	"go.flipt.io/flipt/storage"
+	"google.golang.org/protobuf/proto"
 	empty "google.golang.org/protobuf/types/known/emptypb"
 )
 
 // GetFlag gets a flag
-func (s *Server) GetFlag(ctx context.Context, r *flipt.GetFlagRequest) (*flipt.Flag, error) {
+func (s *Server) GetFlag(ctx context.Context, r *flipt.GetFlagRequest) (flag *flipt.Flag, err error) {
 	s.logger.WithField("request", r).Debug("get flag")
-	// TODO: get with cache
-	flag, err := s.store.GetFlag(ctx, r.Key)
+	if s.cacheEnabled {
+		// check cache
+		flag, err = s.getFlagWithCache(ctx, r)
+	} else {
+		flag, err = s.store.GetFlag(ctx, r.Key)
+	}
 	s.logger.WithField("response", flag).Debug("get flag")
 	return flag, err
+}
+
+func (s *Server) getFlagWithCache(ctx context.Context, r *flipt.GetFlagRequest) (*flipt.Flag, error) {
+	var (
+		logger   = s.logger.WithField("request", r)
+		key, err = flagCacheKey(r)
+	)
+
+	if err != nil {
+		// if error, log and continue to evaluate
+		logger.WithError(err).Error("generating cache key")
+		return s.store.GetFlag(ctx, r.Key)
+	}
+
+	cached, ok, err := s.cache.Get(ctx, key)
+	if err != nil {
+		// if error, log and continue to evaluate
+		logger.WithError(err).Error("getting from cache")
+		return s.store.GetFlag(ctx, r.Key)
+	}
+
+	if !ok {
+		logger.Debug("evaluate cache miss")
+		flag, err := s.store.GetFlag(ctx, r.Key)
+		if err != nil {
+			return flag, err
+		}
+		data, err := proto.Marshal(flag)
+		if err != nil {
+			return flag, err
+		}
+		err = s.cache.Set(ctx, key, data)
+		return flag, err
+	}
+
+	flag := &flipt.Flag{}
+	if err := proto.Unmarshal(cached, flag); err != nil {
+		logger.WithError(err).Error("unmarshalling from cache")
+		return s.store.GetFlag(ctx, r.Key)
+	}
+
+	logger.Debugf("evaluate cache hit: %+v", flag)
+	return flag, nil
 }
 
 // ListFlags lists all flags
@@ -84,4 +134,9 @@ func (s *Server) DeleteVariant(ctx context.Context, r *flipt.DeleteVariantReques
 		return nil, err
 	}
 	return &empty.Empty{}, nil
+}
+
+func flagCacheKey(r *flipt.GetFlagRequest) (string, error) {
+	k := fmt.Sprintf("f:%s", r.GetKey())
+	return fmt.Sprintf("flipt:%x", md5.Sum([]byte(k))), nil //nolint:gosec
 }
