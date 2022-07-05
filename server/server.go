cb1b9cabd2f85ad4205d06071e8f8577f55e8eb1
Mark Phelps <mark@flipt.io>
2022-07-05 11:34:33 -0400

    Rebaising

--- server/server.go
+++ server/server.go
@@ -9,9 +9,6 @@ import (
 	"go.flipt.io/flipt/storage"
 
 	"github.com/sirupsen/logrus"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 var _ flipt.FliptServer = &Server{}
@@ -39,46 +36,4 @@ func New(logger logrus.FieldLogger, store storage.Store, opts ...Option) *Server
 	}
 
 	return s
-}
-
-// ValidationUnaryInterceptor validates incomming requests
-func (s *Server) ValidationUnaryInterceptor(ctx context.Context, req interface{}, _ *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
-	if v, ok := req.(flipt.Validator); ok {
-		if err := v.Validate(); err != nil {
-			return nil, err
-		}
-	}
-
-	return handler(ctx, req)
-}
-
-// ErrorUnaryInterceptor intercepts known errors and returns the appropriate GRPC status code
-func (s *Server) ErrorUnaryInterceptor(ctx context.Context, req interface{}, _ *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
-	resp, err = handler(ctx, req)
-	if err == nil {
-		return resp, nil
-	}
-
-	errorsTotal.Inc()
-
-	var errnf errs.ErrNotFound
-	if errors.As(err, &errnf) {
-		err = status.Error(codes.NotFound, err.Error())
-		return
-	}
-
-	var errin errs.ErrInvalid
-	if errors.As(err, &errin) {
-		err = status.Error(codes.InvalidArgument, err.Error())
-		return
-	}
-
-	var errv errs.ErrValidation
-	if errors.As(err, &errv) {
-		err = status.Error(codes.InvalidArgument, err.Error())
-		return
-	}
-
-	err = status.Error(codes.Internal, err.Error())
-	return
 }
