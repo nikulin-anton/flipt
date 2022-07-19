0d5c58a4d726b92dbb9b666fd3e91cedd7b3c380
Anton Nikulin <a.nikulin@conundrum.ai>
2022-07-19 14:47:14 +0500

    Add baseUrl support

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -10,8 +10,12 @@ export default defineConfig(({ mode }) => {
   const host = process.env.FLIPT_SERVER_HTTP_HOST || "localhost";
   // get FLIPT_SERVER_HTTP_PORT from environment variable or default to 8080
   const port = process.env.FLIPT_SERVER_HTTP_PORT || 8080;
+  // get FLIPT_SERVER_BASE_URL from environment variable or default to /
+  const base = process.env.FLIPT_SERVER_BASE_URL || "/";
+
   return {
     plugins: [vue()],
+    base,
     resolve: {
       extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
       alias: [
